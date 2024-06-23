c4f7a377e3722cab2a37502e3807fb57892de605
Nick Baur <79518844+baurnick@users.noreply.github.com>
2024-06-23 16:11:51 +0200

    DEAR-65: add font and base colors

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Logo from '@/components/Logo/Logo';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
+    <main className="flex min-h-screen flex-col items-center justify-center">
       <Logo />
+      <p className="text-xl">Welcome to yappi</p>
     </main>
   );
 }
