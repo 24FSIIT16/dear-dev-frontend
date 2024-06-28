ead777c6e0ebb8cd3ca04a1e3fdc86f7618d7750
Nick Baur <79518844+baurnick@users.noreply.github.com>
2024-06-28 16:16:32 +0200

    DEAR-99: add basic oauth, github and google

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,41 @@
-import Logo from '@components/ui/Logo/Logo';
 import * as React from 'react';
+import { signIn, signOut } from '@/auth';
+import { Button } from '@components/ui/Buttons/Button';
+import SessionInfo from './SessionInfo';
 
 const Home: React.FC = () => (
   <div className="flex min-h-[calc(100vh-9rem)] flex-col items-center justify-center space-y-2">
-    <Logo className="h-20 w-20 text-black dark:text-white" />
-    <span className="text-xl font-light">yappi</span>
+    <form
+      action={async () => {
+        'use server';
+
+        await signIn('github');
+      }}
+    >
+      <Button>Sign in with GitHub</Button>
+    </form>
+
+    <form
+      action={async () => {
+        'use server';
+
+        await signOut();
+      }}
+    >
+      <Button>SignOut</Button>
+    </form>
+
+    <form
+      action={async () => {
+        'use server';
+
+        await signIn('google');
+      }}
+    >
+      <Button>Sign in with Google</Button>
+    </form>
+
+    <SessionInfo />
   </div>
 );
 
