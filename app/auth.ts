f09b837709f3c639564027a2941d19d145db0aa3
Nick Baur <79518844+baurnick@users.noreply.github.com>
2024-06-28 16:58:53 +0200

    DEAR-99: add pg adapter

--- app/auth.ts
+++ app/auth.ts
@@ -1,7 +1,21 @@
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
 import Google from 'next-auth/providers/google';
+import PostgresAdapter from '@auth/pg-adapter';
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PostgresAdapter(pool),
   providers: [GitHub, Google],
 });
