e0bba61f28d02a304edd5c344fa81f705419ad8c
Nick Baur <79518844+baurnick@users.noreply.github.com>
2024-07-08 19:57:01 +0200

    DEAR-108: add further mock charts

--- app/(main)/dashboard/layout.tsx
+++ app/(main)/dashboard/layout.tsx
@@ -6,9 +6,9 @@ interface DashboardLayoutProps {
 }
 
 const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => (
-  <div className="space-y-8 px-16">
+  <div className="space-y-8 px-16 pb-16">
     <div className="space-y-0.5">
-      <h1>Insights</h1>
+      <h1>Dashboard</h1>
       <p className="text-md font-thin">Gain insights from the happiness data you collected.</p>
     </div>
     <Separator className="dark:border-secondaryBG-dark" />
