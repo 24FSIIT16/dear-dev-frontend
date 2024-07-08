faf26d769f56a672c94ae309f1bda7aba017a8c4
Nick Baur <79518844+baurnick@users.noreply.github.com>
2024-07-08 18:09:59 +0200

    DEAR-108: add first mock charts

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,26 +1,16 @@
 import * as React from 'react';
-import BasicCard from '@components/Cards/Basic';
-import OverallHappiness from '@components/Surveys/OverallHappiness';
-import WorkItemHappiness from '@components/Surveys/WorkItemHappiness';
-import Feedback from '@components/Surveys/Feedback';
+import HappinessLineChart from './components/HappinessLineChart';
+import WorkkindBarChart from './components/WorkkindBarChart';
 
-export default function Dashboard() {
-  return (
-    <main className="grid flex-1 items-start gap-6 p-4 sm:px-6 sm:py-0 md:gap-8">
-      <div className="mx-auto grid max-w-[59rem] flex-1 auto-rows-max gap-6">
-        <div className="flex gap-6">
-          <BasicCard />
-          <div className="flex flex-col gap-6">
-            <OverallHappiness />
-            <BasicCard />
-          </div>
-          <div className="flex flex-col gap-6">
-            <WorkItemHappiness />
-            <Feedback />
-          </div>
-        </div>
-        <BasicCard />
-      </div>
-    </main>
-  );
-}
+const DashboardPage: React.FC = () => (
+  <div className="grid grid-cols-2 gap-10">
+    <div>
+      <WorkkindBarChart />
+    </div>
+    <div>
+      <HappinessLineChart />
+    </div>
+  </div>
+);
+
+export default DashboardPage;
