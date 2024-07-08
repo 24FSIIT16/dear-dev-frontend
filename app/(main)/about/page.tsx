75928e8eb556aed022a9c6106fd66c4e2740522c
Nick Baur <79518844+baurnick@users.noreply.github.com>
2024-07-08 13:08:49 +0200

    DEAR-107: add about page

---
+++ app/(main)/about/page.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import Separator from '@components/ui/Separator/Separator';
+import AboutAccordion from './components/AboutAccordion';
+import TeamHoverCard from './components/TeamHoverCard';
+
+const AboutPage: React.FC = () => (
+  <div>
+    <AboutAccordion />
+    <div className="mt-8 space-y-1">
+      <h2>Team</h2>
+      <p className="text-sm font-thin">Team behind yappi.</p>
+    </div>
+    <div className="mt-4 flex w-64 flex-row justify-between">
+      <TeamHoverCard
+        trigger="@petra"
+        img="/assets/Avatar/Team/petra.svg"
+        mail="petra.kohler@students.fhnw.ch"
+        description="I think about stuff, I desing it, and then I build it."
+      />
+      <TeamHoverCard
+        trigger="@nick"
+        img="/assets/Avatar/Team/nick.svg"
+        mail="nick.baur@students.fhnw.ch"
+        description="I'm a dog person, I like to code and design stuff."
+      />
+    </div>
+    <div className="mt-8 space-y-8">
+      <Separator />
+      <div className="space-y-1">
+        <h2>Contact</h2>
+        <p className="text-sm font-thin">Do you have any questions? - Get in touch.</p>
+      </div>
+    </div>
+  </div>
+);
+
+export default AboutPage;
--- components/Error/Error.tsx
+++ components/Error/Error.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import cn from '@/lib/utils';
 import Link from 'next/link';
-import { RefreshCcw } from 'lucide-react';
+import { LogOut, RefreshCcw } from 'lucide-react';
 import { Button } from '@components/ui/Buttons/Button';
 import Separator from '@components/ui/Separator/Separator';
+import { signOut } from 'next-auth/react';
 
 interface ErrorProps {
   className?: string;
@@ -12,35 +13,48 @@ interface ErrorProps {
   showContact: boolean;
 }
 
-const Error: React.FC<ErrorProps> = ({ className, errorMessage, action, showContact = false }) => (
-  <div
-    className={cn('flex min-h-[calc(100vh-7rem)] items-center justify-center text-black dark:text-white', className)}
-  >
-    <div className="flex flex-col">
-      <h1>Oops, something went wrong!</h1>
-      <p className="text-md font-thin">{errorMessage}</p>
-      {action && (
-        <Link href={action}>
-          <Button variant="outline" className="mt-4 text-xs font-normal">
-            <RefreshCcw className="mr-2 h-4 w-4" />
-            Try again
-          </Button>
-        </Link>
-      )}
-      {showContact && (
-        <>
-          <Separator className="my-3" />
-          <p className="mt-2 text-sm font-thin">
-            If the problem persists, please{' '}
-            <Link href="/contact" className="hover:text-primary underline underline-offset-4">
-              contact us
+const Error: React.FC<ErrorProps> = ({ className, errorMessage, action, showContact = false }) => {
+  const handleLogout = async () => {
+    await signOut();
+  };
+
+  return (
+    <div
+      className={cn('flex min-h-[calc(100vh-7rem)] items-center justify-center text-black dark:text-white', className)}
+    >
+      <div className="flex flex-col">
+        <h1>Oops, something went wrong!</h1>
+        <p className="text-md font-thin">{errorMessage}</p>
+        {action && (
+          <div className="mt-4 flex flex-row items-center space-x-4">
+            <Link href={action}>
+              <Button variant="outline" className="text-xs font-normal">
+                <RefreshCcw className="mr-2 h-4 w-4" />
+                Try again
+              </Button>
             </Link>
-            .
-          </p>
-        </>
-      )}
+            <Separator className="h-8" orientation="vertical" />
+            <Button variant="outline" className="text-xs font-normal" onClick={handleLogout}>
+              <LogOut className="mr-2 h-4 w-4" />
+              Log out
+            </Button>
+          </div>
+        )}
+        {showContact && (
+          <>
+            <Separator className="my-3" />
+            <p className="mt-2 text-sm font-thin">
+              If the problem persists, please{' '}
+              <Link href="/about" className="hover:text-primary underline underline-offset-4">
+                contact us
+              </Link>
+              .
+            </p>
+          </>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Error;
