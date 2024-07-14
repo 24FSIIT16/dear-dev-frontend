1d7714011bc59e5e5ddeb026b48c8dd3234c823d
Nick Baur <79518844+baurnick@users.noreply.github.com>
2024-07-14 17:00:12 +0200

    DEAR-117: add create team dialog

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -2,7 +2,10 @@
 
 import * as React from 'react';
 import { Button } from '@components/ui/Buttons/Button';
+import { Dialog, DialogTrigger, DialogContent } from '@components/ui/Dialog/Dialog';
 import SelectableCard from './components/SelectableCard';
+import PersonalTeamDialog from './components/PersonalTeamDialog';
+import JoinTeamDialog from './components/JoinTeamDialog';
 
 const OnboardingPage: React.FC = () => {
   const [selected, setSelected] = React.useState<string | null>(null);
@@ -11,11 +14,21 @@ const OnboardingPage: React.FC = () => {
     setSelected(option);
   };
 
+  const renderDialogContent = () => {
+    if (selected === 'personal') {
+      return <PersonalTeamDialog />;
+    }
+    if (selected === 'team') {
+      return <JoinTeamDialog />;
+    }
+    return null;
+  };
+
   return (
     <div className="flex flex-col items-center justify-center gap-8 text-center">
       <div className="mb-8">
-        <h1>How to start?</h1>
-        <p>Get started</p>
+        <h1>How would you like to use yappi?</h1>
+        <p>You have just created an account. Choose what to do next.</p>
       </div>
       <div className="mb-4 flex flex-row gap-8">
         <SelectableCard
@@ -23,17 +36,26 @@ const OnboardingPage: React.FC = () => {
           onClick={() => handleSelect('personal')}
           imageSrc="/assets/Illus/personal.svg"
           title="As an individual"
-          description="It's a best option for you to exlpore or do something for all by yourself. Later, you can create a team workspace, that's easy."
+          description="You want to use yappi as an individual, so that you can track your own happiness. 
+          If you want to start a shared team workspace, you should also select this option and invite your team members in a second step. As the initiator, you will have admin rights."
         />
         <SelectableCard
           selected={selected === 'team'}
           onClick={() => handleSelect('team')}
           imageSrc="/assets/Illus/team.svg"
           title="As a part of a team"
-          description="It's a best option for you to exlpore or do something for all by yourself. Later, you can create a team workspace, that's easy."
+          description="You only select this option if you have been invited to a team workspace and have received a four-digit code. 
+          In this workspace, you will see your individual trackings as well as an overview of the team happiness."
         />
       </div>
-      <Button className="bg-primaryBlue-main px-8 hover:bg-primaryBlue-main">Continue</Button>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button disabled={!selected} className="bg-primaryBlue-main px-8 hover:bg-primaryBlue-main">
+            Continue
+          </Button>
+        </DialogTrigger>
+        <DialogContent>{renderDialogContent()}</DialogContent>
+      </Dialog>
     </div>
   );
 };
