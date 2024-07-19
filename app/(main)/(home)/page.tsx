45cc30a5aafca7289491759455af7f77f1ab7439
Nick Baur <79518844+baurnick@users.noreply.github.com>
2024-07-19 11:31:56 +0200

    DEAR-117: fix loading on home page

--- app/(main)/(home)/page.tsx
+++ app/(main)/(home)/page.tsx
@@ -25,26 +25,28 @@ const Home: React.FC = () => {
   if (isLoading) return <Loading />;
   if (error) return <Error errorMessage="It seems there was a problem loading your account." action="/" showContact />;
 
-  return (
-    <div className="space-y-4">
-      <div className="flex w-full">
-        <h1>{user ? `Welcome, ${user?.name}` : 'Welcome'}</h1>
+  if (user && user.hasTeam) {
+    return (
+      <div className="space-y-4">
+        <div className="flex w-full">
+          <h1>{user ? `Welcome, ${user?.name}` : 'Welcome'}</h1>
+        </div>
+        <Alert variant="informative">
+          <Megaphone className="h-4 w-4" />
+          <AlertTitle>We think you forgot something?</AlertTitle>
+          <AlertDescription>You have not tracked your Happines since 2 days.</AlertDescription>
+        </Alert>
+        <div className="grid grid-cols-2 gap-10">
+          <OverallHappiness />
+          <OverallHappinessWeather />
+        </div>
+        <div className="grid grid-cols-2 gap-10">
+          <WorkItemHappiness />
+          <Feedback />
+        </div>
       </div>
-      <Alert variant="informative">
-        <Megaphone className="h-4 w-4" />
-        <AlertTitle>We think you forgot something?</AlertTitle>
-        <AlertDescription>You have not tracked your Happines since 2 days.</AlertDescription>
-      </Alert>
-      <div className="grid grid-cols-2 gap-10">
-        <OverallHappiness />
-        <OverallHappinessWeather />
-      </div>
-      <div className="grid grid-cols-2 gap-10">
-        <WorkItemHappiness />
-        <Feedback />
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Home;
