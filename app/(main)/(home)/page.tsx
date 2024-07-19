223a8dce61193cf88647d97766b8acdc31a8ba1a
Nick Baur <79518844+baurnick@users.noreply.github.com>
2024-07-19 11:39:26 +0200

    DEAR-117: fix error

--- app/(main)/(home)/page.tsx
+++ app/(main)/(home)/page.tsx
@@ -25,28 +25,32 @@ const Home: React.FC = () => {
   if (isLoading) return <Loading />;
   if (error) return <Error errorMessage="It seems there was a problem loading your account." action="/" showContact />;
 
-  if (user && user.hasTeam) {
-    return (
-      <div className="space-y-4">
-        <div className="flex w-full">
-          <h1>{user ? `Welcome, ${user?.name}` : 'Welcome'}</h1>
+  return (
+    <div>
+      {user && user.hasTeam ? (
+        <div className="space-y-4">
+          <div className="flex w-full">
+            <h1>{user ? `Welcome, ${user?.name}` : 'Welcome'}</h1>
+          </div>
+          <Alert variant="informative">
+            <Megaphone className="h-4 w-4" />
+            <AlertTitle>We think you forgot something?</AlertTitle>
+            <AlertDescription>You have not tracked your Happiness since 2 days.</AlertDescription>
+          </Alert>
+          <div className="grid grid-cols-2 gap-10">
+            <OverallHappiness />
+            <OverallHappinessWeather />
+          </div>
+          <div className="grid grid-cols-2 gap-10">
+            <WorkItemHappiness />
+            <Feedback />
+          </div>
         </div>
-        <Alert variant="informative">
-          <Megaphone className="h-4 w-4" />
-          <AlertTitle>We think you forgot something?</AlertTitle>
-          <AlertDescription>You have not tracked your Happines since 2 days.</AlertDescription>
-        </Alert>
-        <div className="grid grid-cols-2 gap-10">
-          <OverallHappiness />
-          <OverallHappinessWeather />
-        </div>
-        <div className="grid grid-cols-2 gap-10">
-          <WorkItemHappiness />
-          <Feedback />
-        </div>
-      </div>
-    );
-  }
+      ) : (
+        <Loading />
+      )}
+    </div>
+  );
 };
 
 export default Home;
