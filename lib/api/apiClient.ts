0ecd749fc4b27dab75b95f5159a3479084c683ae
Nick Baur <79518844+baurnick@users.noreply.github.com>
2024-07-27 10:20:18 +0200

    Add api base url

--- lib/api/apiClient.ts
+++ lib/api/apiClient.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const setHeaders = (token: string) => ({
   Authorization: `Bearer ${token}`,
