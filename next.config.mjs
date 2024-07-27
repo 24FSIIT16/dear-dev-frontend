7818b976603aea019233de7a928db97790c2c1c9
Nick Baur <79518844+baurnick@users.noreply.github.com>
2024-07-27 16:51:55 +0200

    Api base (#37)
    * fix: revert github workflow
    
    * Add api base url
    
    * Update ci-cd.yml
    
    * Update ci-cd.yml
    
    * Add publi api key to next config
    
    * Fix next public api url
    
    * Add NEXT_PUBLIC_API_URL
    
    * Update pipeline
    
    * Update ci-cd.yml

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['media.giphy.com'],
   },
+  env: {
+    NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+  },
 };
 
 export default nextConfig;
